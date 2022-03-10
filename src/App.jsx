145cc340ff32eff86d228e4ac40505a6d5132e6d
pooriaahmadi <pooriababak444@gmail.com>
2022-03-10 16:43:51 -0500

    :)

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
 import React from "react";
 import "./App.css";
 import Header from "./Components/Header/Header";
-
-function App() {
-  return (
-    <div className="App">
-      <Header></Header>
-    </div>
-  );
-}
+import { Routes, BrowserRouter, Route } from "react-router-dom";
+const App = () => {
+	return (
+		<div className="App">
+			<BrowserRouter>
+				<Header></Header>
+				<Routes>
+					<Route path="/" element={<h1>HOME PAGE</h1>} />
+					<Route path="/1" element={<h1>ABOUT US</h1>} />
+					<Route path="/2" element={<h1>CONTACT US</h1>} />
+				</Routes>
+			</BrowserRouter>
+		</div>
+	);
+};
 
 export default App;
--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,16 +1,16 @@
 import HeaderItem from "./HeaderItem";
 
 const Header = () => {
-  return (
-    <header>
-      <div className="left">
-        <h1>Manto</h1>
-        <HeaderItem url="home" title="Home"></HeaderItem>
-      </div>
-      <div className="right">
-        <img src="" alt="" />
-      </div>
-    </header>
-  );
+	return (
+		<header>
+			<div className="left">
+				<h1>Manto</h1>
+				<HeaderItem url="home" title="Home"></HeaderItem>
+			</div>
+			<div className="right">
+				<img src="" alt="" />
+			</div>
+		</header>
+	);
 };
 export default Header;
