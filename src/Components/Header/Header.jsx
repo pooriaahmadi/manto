f475e86d2eed8e8b3fe26734d3817e0a0753df80
sydeich <sydeich@gmail.com>
2022-03-10 20:04:10 -0500

    :D

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,16 +1,16 @@
 import HeaderItem from "./HeaderItem";
-
+import logo from "../../assets/images/logo.png";
 const Header = () => {
-	return (
-		<header>
-			<div className="left">
-				<h1>Manto</h1>
-				<HeaderItem url="home" title="Home"></HeaderItem>
-			</div>
-			<div className="right">
-				<img src="" alt="" />
-			</div>
-		</header>
-	);
+  return (
+    <header>
+      <div className="left">
+        <h1>Manto</h1>
+        <HeaderItem url="home" title="Home"></HeaderItem>
+      </div>
+      <div className="right">
+        <img src={logo} alt="" />
+      </div>
+    </header>
+  );
 };
 export default Header;
