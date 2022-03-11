6c8623407259f55b27daddb372ab2b73394e942e
pooriaahmadi <pooriababak444@gmail.com>
2022-03-11 16:11:37 -0500

    mobile nerds

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,21 @@
 import React from "react";
 import "./App.css";
+import "./assets/scss/responsive.scss";
 import Header from "./Components/Header/Header";
 import { Routes, BrowserRouter, Route } from "react-router-dom";
 const App = () => {
-    return (
-        <div className="app">
-            <BrowserRouter>
-                <Header></Header>
-                <Routes>
-                    <Route path="/" element={<h1>HOME PAGE</h1>} />
-                    <Route path="/1" element={<h1>ABOUT US</h1>} />
-                    <Route path="/2" element={<h1>CONTACT US</h1>} />
-                </Routes>
-            </BrowserRouter>
-        </div>
-    );
+	return (
+		<div className="app">
+			<BrowserRouter>
+				<Header></Header>
+				<Routes>
+					<Route path="/" element={<h1>HOME PAGE</h1>} />
+					<Route path="/1" element={<h1>ABOUT US</h1>} />
+					<Route path="/2" element={<h1>CONTACT US</h1>} />
+				</Routes>
+			</BrowserRouter>
+		</div>
+	);
 };
 
 export default App;
