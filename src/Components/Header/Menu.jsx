c97dd4ecf6020d1191e74253258e979ce6c13950
pooriaahmadi <pooriababak444@gmail.com>
2022-03-11 15:36:30 -0500

    aaaaaaaaaa

--- src/Components/Header/Menu.jsx
+++ src/Components/Header/Menu.jsx
@@ -1,16 +1,17 @@
 import HeaderItem from "./HeaderItem";
 import close from "../../assets/images/close.png";
+import "../../assets/scss/menu.scss";
 
 const Menu = () => {
-    return (
-        <div className="menu">
-            <div className="top">
-                <h1>Manto</h1> <img src={close} alt="" />
-            </div>
-            <div className="bottom">
-                <HeaderItem url="/" title="home" />
-            </div>
-        </div>
-    );
+	return (
+		<div className="menu">
+			<div className="top">
+				<h1>Manto</h1> <img src={close} alt="" />
+			</div>
+			<div className="bottom">
+				<HeaderItem url="/" title="Home" />
+			</div>
+		</div>
+	);
 };
 export default Menu;
