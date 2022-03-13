c2b0c04cf5689caa4d9632a377f2e38314c7dbee
pooriaahmadi <pooriababak444@gmail.com>
2022-03-13 11:12:20 -0400

    typo

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -5,30 +5,30 @@ import Menu from "./Menu";
 import hamMenu from "../../assets/images/menu.png";
 import { useState } from "react";
 const Header = () => {
-    const [active, setActive] = useState(false);
-    const toggleActive = () => {
-        if (active) {
-            setActive(false);
-        } else {
-            setActive(true);
-        }
-    };
-    return (
-        <>
-            <header>
-                <div className="left">
-                    <h1>Manto</h1>
-                    <div className="items">
-                        <HeaderItem url="home" title="Home"></HeaderItem>
-                    </div>
-                </div>
-                <div className="right">
-                    <img src={logo} alt="" />
-                    <img src={hamMenu} onClick={toggleActive} alt="" />
-                </div>
-            </header>
-            <Menu active={active} toggleActive={toggleActive}></Menu>
-        </>
-    );
+	const [active, setActive] = useState(false);
+	const toggleActive = () => {
+		if (active) {
+			setActive(false);
+		} else {
+			setActive(true);
+		}
+	};
+	return (
+		<>
+			<header>
+				<div className="left">
+					<h1>Manto</h1>
+					<div className="items">
+						<HeaderItem url="/" title="Home"></HeaderItem>
+					</div>
+				</div>
+				<div className="right">
+					<img src={logo} alt="" />
+					<img src={hamMenu} onClick={toggleActive} alt="" />
+				</div>
+			</header>
+			<Menu active={active} toggleActive={toggleActive}></Menu>
+		</>
+	);
 };
 export default Header;
