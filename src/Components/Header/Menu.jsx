ac071b48b9ff52ec1167797e8b147112aa10b894
pooriaahmadi <pooriababak444@gmail.com>
2022-03-15 10:06:15 -0700

    Menu element

--- src/Components/Header/Menu.jsx
+++ src/Components/Header/Menu.jsx
@@ -3,15 +3,16 @@ import close from "../../assets/images/close.png";
 import "../../assets/scss/menu.scss";
 
 const Menu = ({ active, toggleActive }) => {
-    return (
-        <div className={`menu` + (active ? " active" : "")}>
-            <div className="top">
-                <h1>Manto</h1> <img src={close} onClick={toggleActive} alt="" />
-            </div>
-            <div className="bottom">
-                <HeaderItem url="/" title="Home" />
-            </div>
-        </div>
-    );
+	return (
+		<div className={`menu` + (active ? " active" : "")}>
+			<div className="top">
+				<h1>Manto</h1> <img src={close} onClick={toggleActive} alt="" />
+			</div>
+			<div className="bottom">
+				<HeaderItem url="/" title="Home" />
+				<HeaderItem url="/admin" title="Admin" />
+			</div>
+		</div>
+	);
 };
 export default Menu;
