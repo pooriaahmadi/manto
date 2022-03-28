d41d7a0ae93d0787ac847a55acea7acbae535d9b
pooriaahmadi <pooriababak444@gmail.com>
2022-03-27 17:55:22 -0700

    controls placeholder

--- src/Pages/Admin.jsx
+++ src/Pages/Admin.jsx
@@ -1,12 +1,27 @@
 import TeamsInline from "../Components/Teams/TeamsInline";
 import UsersInline from "../Components/Users/UsersInline";
 import CategoriesInline from "../Components/Categories/CategoriesInline";
+import { Link } from "react-router-dom";
 import "../assets/scss/admin.scss";
 
 const Admin = ({ database }) => {
 	return (
 		<div className="admin">
 			<div className="teams">
+				<div className="actions">
+					<Link className="action" to="/queue/load">
+						Queue Load
+					</Link>
+					<Link className="action" to="/queue/load">
+						Queue Load
+					</Link>
+					<Link className="action" to="/queue/load">
+						Queue Load
+					</Link>
+					<Link className="action" to="/queue/load">
+						Queue Load
+					</Link>
+				</div>
 				<TeamsInline database={database}></TeamsInline>
 				<UsersInline database={database}></UsersInline>
 				<CategoriesInline database={database}></CategoriesInline>
