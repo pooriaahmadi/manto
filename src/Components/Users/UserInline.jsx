c31fd0ff2a172c4079f8b6bdf785b72d6247db47
pooriaahmadi <pooriababak444@gmail.com>
2022-04-14 21:18:17 +0430

    more updates

--- src/Components/Users/UserInline.jsx
+++ src/Components/Users/UserInline.jsx
@@ -1,12 +1,17 @@
 import "../../assets/scss/teaminline.scss";
 import { Link } from "react-router-dom";
 const UserInline = ({ username, name, id, handleDelete }) => {
+	const use = () => {
+		localStorage.setItem("user", id);
+		alert("done");
+	};
 	return (
 		<div className="team-inline">
 			<h3>{name}</h3>
 			<div className="controls">
 				<Link to={`/users/${id}/qrcode`}>QRCode</Link>
 				<button onClick={handleDelete}>Delete</button>
+				<button onClick={handleDelete}>Use</button>
 			</div>
 		</div>
 	);
